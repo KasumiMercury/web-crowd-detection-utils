222523986e814361c61737259803e19e9a662f6e
KasumiMercury <88318012+KasumiMercury@users.noreply.github.com>
2026-05-06 18:56:16 +0900

    style: format

--- example/yolo-webcam/vite.config.ts
+++ example/yolo-webcam/vite.config.ts
@@ -8,8 +8,8 @@ export default defineConfig({
 		// onnxruntime-webがort.bundle.min.mjs内でWASMファイルをnew URL(..., import.meta.url)で読み込んでいる
 		// 事前バンドルするとURLが書き換えられてWASMアセットの検出ができなくなる
 		// 事前バンドルの対象から外す
-    exclude: ["onnxruntime-web"],
-  },
+		exclude: ["onnxruntime-web"],
+	},
 	server: {
 		// SharedArrayBufferを有効にするためのCross Origin Isolationの設定
 		// wasmをmulti-threadedで動かすために必要
